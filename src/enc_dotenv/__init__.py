42fda7e8e7f8eb04b3391b57386ce6c55b6acabd
bhags1981 <jungsinyu@gmail.com>
2019-12-13 11:27:44 +0900

    added load_encrypted_dotenv

--- src/enc_dotenv/__init__.py
+++ src/enc_dotenv/__init__.py
@@ -1,4 +1,4 @@
-from enc_dotenv.main import encrypt_dotenv
+from enc_dotenv.main import encrypt_dotenv, load_encrypted_dotenv
 
 # __copyright__ = 'Copyright (C) 2019 Sinyu Jung'
 # __version__ = '1.0.0'
@@ -7,4 +7,4 @@ from enc_dotenv.main import encrypt_dotenv
 # __author_email__ = 'jungsinyu@gmail.com'
 # __url__ = 'http://github.com/account/repository'
 
-__all__ = ['encrypt_dotenv']
+__all__ = ['encrypt_dotenv', 'load_encrypted_dotenv']
