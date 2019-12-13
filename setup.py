4b9c53485976df1c3d9679dc9d76b9fb559457f5
bhags1981 <jungsinyu@gmail.com>
2019-12-13 11:27:06 +0900

    added install requires

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ def read_files(files):
     return "\n".join(data)
 
 
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
 long_description = read_files(['README.md', 'CHANGELOG.md'])
 
 meta = {}
@@ -25,24 +29,29 @@ setup(
     author="Sinyu Jung",
     author_email="",
     url="https://github.com/bhags1981/python-encrypted-dotenv",
-    keywords=['encrypted', 'environment variables', 'deployments', 'settings', 'env', 'dotenv',
+    keywords=['encrypted　environment variables', 'deployments', 'settings', 'env', 'dotenv',
               'configurations', 'python'],
     packages=['enc_dotenv'],
     package_dir={'': 'src'},
     package_data={
         'enc_dotenv': ['py.typed'],
     },
-    extras_require={
-        'cli': ['click>=5.0', ],
-    },
+    install_requires=[
+        "python-dotenv",
+        "pyAesCrypt"
+    ],
+
+    # extras_require={
+    #     'cli': ['click>=5.0', ],
+    # },
     # entry_points='''
     #     [console_scripts]
     #     dotenv=dotenv.cli:cli
     # ''',
     classifiers=[
         # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 1 - Planning',
-        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
         # 'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
@@ -80,3 +89,4 @@ setup(
 #     Queries directly to my email are likely to receive a canned response.
 #
 #     Many thanks for your understanding.
+# repository: https://upload.pypi.org/legacy/
